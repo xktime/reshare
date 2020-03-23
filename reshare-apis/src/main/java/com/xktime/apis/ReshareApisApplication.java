1988f7ce6a3e17fc30efe8d29dfcb60918759c5e
xktime <luoliwen0913@foxmail.com>
2020-03-23 16:44:52 +0800

    修改：文章服务

--- reshare-apis/src/main/java/com/xktime/apis/ReshareApisApplication.java
+++ reshare-apis/src/main/java/com/xktime/apis/ReshareApisApplication.java
@@ -1,4 +1,4 @@
-package com.xktime.reshareapis;
+package com.xktime.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ reshare-article/src/main/java/com/xktime/article/ReshareArticleApplication.java
@@ -0,0 +1,13 @@
+package com.xktime.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReshareArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReshareArticleApplication.class, args);
+    }
+
+}
