9322ea9df3da0bb8812c86b074140f3c3f26278b
xktime <luoliwen0913@foxmail.com>
2020-04-01 16:49:24 +0800

    添加behavior模块

---
+++ reshare-behavior/src/test/java/com/xktime/behavior/BehaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xktime.behavior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehaviorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
