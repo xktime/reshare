9dd927b47a0e5108ddae57f5a7e1a69d6c6a2fce
xktime <luoliwen0913@foxmail.com>
2020-04-21 21:23:19 +0800

    admin前端页面修改

--- reshare-model/src/main/java/com/xktime/model/article/pojos/Article.java
+++ reshare-model/src/main/java/com/xktime/model/article/pojos/Article.java
@@ -1,6 +1,7 @@
 package com.xktime.model.article.pojos;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -19,6 +20,7 @@ public class Article {
     private Integer collection;
     private Integer comment;
     private Integer views;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date publishTime;
     private String origin;
     private String content;
--- reshare-web/src/components/admin/Admin.vue
+++ reshare-web/src/components/admin/Admin.vue
@@ -28,9 +28,11 @@
 
       <el-main>
         <el-table :data="tableData">
-          <el-table-column prop="publishTime" label="日期" width="140"></el-table-column>
-          <el-table-column prop="authorName" label="姓名" width="120"></el-table-column>
-          <el-table-column prop="channelName" label="地址"></el-table-column>
+          <el-table-column prop="title" label="标题" width="250"></el-table-column>
+          <el-table-column prop="authorName" label="作者" width="120"></el-table-column>
+          <el-table-column prop="origin" label="来源" width="120"></el-table-column>
+          <el-table-column prop="url" label="文章地址" width="300"></el-table-column>
+          <el-table-column prop="publishTime" label="发布时间" width="180"></el-table-column>
         </el-table>
       </el-main>
     </el-container>
