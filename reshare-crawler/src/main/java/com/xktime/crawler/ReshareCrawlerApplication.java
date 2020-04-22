bff38f44afd9d397046984c5594c7dee8be884de
xktime <luoliwen0913@foxmail.com>
2020-04-22 20:21:38 +0800

    修改CrawlerArticle插入问题

--- reshare-crawler/src/main/java/com/xktime/crawler/ReshareCrawlerApplication.java
+++ reshare-crawler/src/main/java/com/xktime/crawler/ReshareCrawlerApplication.java
@@ -3,7 +3,7 @@ package com.xktime.crawler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.xktime.common", "com.xktime.crawler"})
+@SpringBootApplication(scanBasePackages = {"com.xktime.common", "com.xktime.crawler", "com.xktime.article"})
 public class ReshareCrawlerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReshareCrawlerApplication.class, args);
