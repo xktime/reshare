b8b64b4391df8ccca9cd0d3a0d548540da4430d5
xktime <luoliwen0913@foxmail.com>
2020-04-22 21:26:31 +0800

    admin页面文章内容展示

--- reshare-model/src/main/java/com/xktime/model/article/dtos/ArticleHomeDto.java
+++ reshare-model/src/main/java/com/xktime/model/article/dtos/ArticleHomeDto.java
@@ -6,18 +6,10 @@ import java.util.Date;
 @Data
 public class ArticleHomeDto {
 
-    // 省市
-    Integer provinceId;
-    // 市区
-    Integer cityId;
-    // 区县
-    Integer countyId;
-    // 最大时间
-    Date maxBehotTime;
-    // 最小时间
-    Date minBehotTime;
     // 分页size
-    Integer size;
+    int size;
+    //当前页码
+    int page;
     // 数据范围，比如频道ID
     String tag;
 
--- reshare-web/src/components/admin/Admin.vue
+++ reshare-web/src/components/admin/Admin.vue
@@ -1,9 +1,9 @@
 <template>
-  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-container style="height: 600px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
       <el-menu :default-openeds="['1']">
         <el-submenu index="1">
-          <template slot="title"><i class="el-icon-setting"></i>文章管理</template>
+          <template slot="title"><i class="el-icon-setting"></i>文章审核</template>
           <el-menu-item-group>
             <template slot="title">审核</template>
             <el-menu-item v-on:click="loadCrawlerArticle()">爬取文章</el-menu-item>
@@ -27,13 +27,31 @@
       </el-header>
 
       <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="title" label="标题" width="250"></el-table-column>
-          <el-table-column prop="authorName" label="作者" width="120"></el-table-column>
-          <el-table-column prop="url" label="文章地址" width="300"></el-table-column>
-          <el-table-column prop="origin" label="来源" width="120"></el-table-column>
-          <el-table-column prop="publishTime" label="发布时间" width="180"></el-table-column>
-          <el-table-column prop="status" label="状态" width="80" :formatter="statusFormatter"></el-table-column>
+        <el-table :data="tableData" height="490">
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <el-form label-position="left" inline class="demo-table-expand">
+                  <iframe v-bind:srcdoc="props.row.content"  width="1400" height="300" frameborder="0"></iframe>
+              </el-form>
+            </template>
+          </el-table-column>
+          <el-table-column prop="title" label="标题" width="300" sortable></el-table-column>
+          <el-table-column prop="authorName" label="作者" width="120" sortable></el-table-column>
+          <el-table-column prop="url" label="文章地址" width="300" sortable></el-table-column>
+          <el-table-column prop="origin" label="来源" width="120" sortable></el-table-column>
+          <el-table-column prop="publishTime" label="发布时间" width="180" sortable></el-table-column>
+          <el-table-column prop="status" label="状态" width="100" :formatter="statusFormatter" sortable></el-table-column>
+          <el-table-column fixed="right" label="操作" >
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">通过</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">失败</el-button>
+            </template>
+          </el-table-column>
         </el-table>
       </el-main>
     </el-container>
@@ -56,7 +74,7 @@
   export default {
     data() {
       return {
-        tableData: []
+        tableData: [],
       }
     },
     methods: {
