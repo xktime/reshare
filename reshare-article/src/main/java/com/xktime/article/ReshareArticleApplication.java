814239648067cb7e9fbf1c8b5bb5c7a994d829ee
unknown <luoliwen0913@foxmail.com>
2020-04-27 14:46:28 +0800

    整合eureka

--- reshare-article/src/main/java/com/xktime/article/ReshareArticleApplication.java
+++ reshare-article/src/main/java/com/xktime/article/ReshareArticleApplication.java
@@ -2,8 +2,10 @@ package com.xktime.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = {"com.xktime.common","com.xktime.article"})
+@EnableEurekaClient
 public class ReshareArticleApplication {
 
     public static void main(String[] args) {
