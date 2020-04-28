2996f2488f1eb1611db74821132f24f33ead637d
unknown <luoliwen0913@foxmail.com>
2020-04-28 20:45:34 +0800

    版本控制整理

--- reshare-apis/src/main/java/com/xktime/apis/admin/AdminController.java
+++ reshare-apis/src/main/java/com/xktime/apis/admin/AdminController.java
@@ -1,16 +1,12 @@
 package com.xktime.apis.admin;
 
-import com.xktime.article.service.CrawlerArticleService;
 import com.xktime.model.article.dtos.ArticleHomeDto;
-import com.xktime.model.article.pojos.CrawlerArticle;
 import com.xktime.model.common.dtos.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("admin")
