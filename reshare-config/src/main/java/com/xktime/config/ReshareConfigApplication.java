01b8d09ecd9ed4e173cb6e94efa14a2239837694
luoliwen <luoliwen0913@foxmail.com>
2020-05-01 00:23:18 +0800

    配置文件测试

--- reshare-config/src/main/java/com/xktime/config/ReshareConfigApplication.java
+++ reshare-config/src/main/java/com/xktime/config/ReshareConfigApplication.java
@@ -2,8 +2,10 @@ package com.xktime.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ReshareConfigApplication {
 
     public static void main(String[] args) {
