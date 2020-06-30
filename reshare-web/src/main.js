c8b2959bbf9af10133c0e4ed8241bcf5c5a9102b
yanzijie <369649972@qq.com>
2020-06-30 17:21:11 +0800

    添加：文章无限滚动加载

--- reshare-web/src/main.js
+++ reshare-web/src/main.js
@@ -7,11 +7,14 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
+import elTableInfiniteScroll from 'el-table-infinite-scroll';
 
 Vue.config.productionTip = false;
 
 //引入axios
 Vue.use(VueAxios, axios);
+
+Vue.use(elTableInfiniteScroll);
 
 //引入Element框架
 Vue.use(ElementUI);
