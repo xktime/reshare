dc3a12325559597b0a50d92d846cc8bff7ab2e8c
yanzijie <369649972@qq.com>
2020-07-14 18:26:22 +0800

    ...

--- reshare-web/src/components/admin/Login.vue
+++ reshare-web/src/components/admin/Login.vue
@@ -1,31 +1,15 @@
 <template>
   <div class="login-container">
-    <el-form :model="ruleForm2" :rules="rules2"
-             status-icon
-             ref="ruleForm2"
-             label-position="left"
-             label-width="0px"
+    <el-form :model="ruleForm2" :rules="rules2" status-icon ref="ruleForm2" label-position="left" label-width="0px"
              class="demo-ruleForm login-page">
       <h3 class="title">系统登录</h3>
       <el-form-item prop="username">
-        <el-input type="text"
-                  v-model="ruleForm2.username"
-                  auto-complete="off"
-                  placeholder="用户名"
-        ></el-input>
+        <el-input type="text" v-model="ruleForm2.username" auto-complete="off" placeholder="用户名"></el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input type="password"
-                  v-model="ruleForm2.password"
-                  auto-complete="off"
-                  placeholder="密码"
-        ></el-input>
+        <el-input type="password" v-model="ruleForm2.password" auto-complete="off" placeholder="密码"></el-input>
       </el-form-item>
-      <el-checkbox
-        v-model="checked"
-        class="rememberme"
-      >记住密码
-      </el-checkbox>
+      <el-checkbox v-model="checked" class="rememberme">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
         <el-button type="primary" style="width:100%;" @click="handleSubmit" :loading="logining">登录</el-button>
       </el-form-item>
