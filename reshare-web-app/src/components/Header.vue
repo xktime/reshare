2c835820429765f5c8ef51bffb2a5f59a8fd9c20
xktime <luoliwen0913@foxmail.com>
2020-09-17 19:00:38 +0800

    页头修改

--- reshare-web-app/src/components/Header.vue
+++ reshare-web-app/src/components/Header.vue
@@ -1,40 +1,42 @@
 <template>
 
   <el-container>
-      <el-aside width="28%"></el-aside>
-      <el-container>
-        <el-input style="width: 350px;"
-                  placeholder="请输入内容"
-                  prefix-icon="el-icon-search"
-                  v-model="search"
-                  size="small">
-        </el-input>
-        <el-button type="primary" icon="el-icon-search" size="small">搜索</el-button>
-        <el-aside width="20%"></el-aside>
-        <el-dropdown class="header-link" v-if="this.$store.state.loging" trigger="click">
+    <el-aside width="25%"></el-aside>
+    <el-link class="header-link" type="primary" href="/" :underline="false">Reshare</el-link>
+    <el-aside width="2%"></el-aside>
+    <el-container>
+      <el-input style="width: 350px;"
+                placeholder="请输入内容"
+                prefix-icon="el-icon-search"
+                v-model="search"
+                size="small">
+      </el-input>
+      <el-button type="primary" icon="el-icon-search" size="small">搜索</el-button>
+      <el-aside width="20%"></el-aside>
+      <el-dropdown class="header-link" v-if="this.$store.state.loging" trigger="click">
             <span class="el-dropdown-link">
               我的<i class="el-icon-arrow-down el-icon--right"></i>
             </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>
-              <el-link href="http://www.baidu.com" :underline="false">发布</el-link>
-            </el-dropdown-item>
-            <el-dropdown-item>
-              <el-link href="http://www.baidu.com" :underline="false">设置</el-link>
-            </el-dropdown-item>
-            <el-dropdown-item>
-              <el-link href="http://www.baidu.com" :underline="false">时间轴</el-link>
-            </el-dropdown-item>
-            <el-dropdown-item>
-              <el-link href="http://www.baidu.com" :underline="false">退出</el-link>
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <div v-else>
-          <el-link class="header-link" type="primary" href="/#/login" :underline="false">登录</el-link>
-          <el-link class="header-link" type="primary" href="http://www.baidu.com" :underline="false">注册</el-link>
-        </div>
-      </el-container>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>
+            <el-link href="http://www.baidu.com" :underline="false">发布</el-link>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <el-link href="http://www.baidu.com" :underline="false">设置</el-link>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <el-link href="http://www.baidu.com" :underline="false">时间轴</el-link>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <el-link href="http://www.baidu.com" :underline="false">退出</el-link>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <div v-else>
+        <el-link class="header-link" type="primary" href="/#/login" :underline="false">登录</el-link>
+        <el-link class="header-link" type="primary" href="http://www.baidu.com" :underline="false">注册</el-link>
+      </div>
+    </el-container>
   </el-container>
 </template>
 
