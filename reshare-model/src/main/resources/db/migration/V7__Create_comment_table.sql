91da1e4a0c11fa40c5a4b6c02f7875e050753866
xktime <luoliwen0913@foxmail.com>
2020-12-17 18:45:22 +0800

    Comment建表语句问题修改

--- reshare-model/src/main/resources/db/migration/V7__Create_comment_table.sql
+++ reshare-model/src/main/resources/db/migration/V7__Create_comment_table.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `user_article_list` (
+CREATE TABLE `comment` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `author_id` bigint unsigned NOT NULL COMMENT '评论作者的ID',
   `bind_id` bigint unsigned NOT NULL COMMENT '绑定ID(文章为article_id)',
@@ -8,5 +8,5 @@ CREATE TABLE `user_article_list` (
   `type` tinyint(1) unsigned DEFAULT '0' COMMENT '评论类型',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `author_id`(`author_id`) USING BTREE,
-  INDEX `article_id`(`article_id`) USING BTREE
+  INDEX `bind_id`(`bind_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='文章评论列表';
