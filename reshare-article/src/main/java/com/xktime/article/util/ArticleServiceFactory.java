e52c96a723c9eb0b3fa1413d8fc44ddd347169bb
xktime <luoliwen0913@foxmail.com>
2020-12-24 17:17:17 +0800

    重构ArticleType为int类型

--- reshare-article/src/main/java/com/xktime/article/util/ArticleServiceFactory.java
+++ reshare-article/src/main/java/com/xktime/article/util/ArticleServiceFactory.java
@@ -2,7 +2,6 @@ package com.xktime.article.util;
 
 import com.xktime.article.service.BaseArticleService;
 import com.xktime.model.article.enums.ArticleTypeEnum;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -18,15 +17,18 @@ public class ArticleServiceFactory {
     @Qualifier("CrawlerArticle")
     BaseArticleService crawlerBaseArticleService;
 
-    public BaseArticleService getService(String articleType) {
-        if (StringUtils.isEmpty(articleType)) {
+    public BaseArticleService getService(int articleType) {
+        ArticleTypeEnum typeEnum = ArticleTypeEnum.getEnum(articleType);
+        if (typeEnum == null) {
             return null;
         }
-        if (articleType.equals(ArticleTypeEnum.CRAWLER_ARTICLE.getDec())) {
-            return crawlerBaseArticleService;
-        } else if (articleType.equals(ArticleTypeEnum.ORIGINAL_ARTICLE.getDec())) {
-            return originalBaseArticleService;
+        switch (typeEnum) {
+            case CRAWLER_ARTICLE:
+                return crawlerBaseArticleService;
+            case ORIGINAL_ARTICLE:
+                return originalBaseArticleService;
+            default:
+                return null;
         }
-        return null;
     }
 }
--- reshare-model/src/main/java/com/xktime/model/article/dtos/c2s/VerifyDto.java
+++ reshare-model/src/main/java/com/xktime/model/article/dtos/c2s/VerifyDto.java
@@ -12,5 +12,5 @@ public class VerifyDto {
     @ApiModelProperty("文章状态 0为未审核 1为未通过 2为通过")
     int status;
     @ApiModelProperty("加载文章的类型")
-    String type;
+    int type;
 }
