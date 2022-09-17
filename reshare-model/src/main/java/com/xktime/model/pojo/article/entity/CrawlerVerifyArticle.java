505b6e7ee48e10c69a6de9ef0a22557423f8842c
xktime <luoliwen0913@foxmail.com>
2022-09-17 20:39:51 +0800

    注册中心pom文件修改

--- reshare-model/src/main/java/com/xktime/model/pojo/article/entity/CrawlerVerifyArticle.java
+++ reshare-model/src/main/java/com/xktime/model/pojo/article/entity/CrawlerVerifyArticle.java
@@ -1,7 +1,6 @@
 package com.xktime.model.pojo.article.entity;
 
 import lombok.Data;
-import lombok.ToString;
 import org.springframework.data.mongodb.core.mapping.Document;
 //import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * 爬虫爬取的文章
  */
 @Data
-@Document(collection = "RoleInfo")
+@Document(collection = "CrawlerVerifyArticle")
 public class CrawlerVerifyArticle extends BaseVerifyArticle {
 
 }
