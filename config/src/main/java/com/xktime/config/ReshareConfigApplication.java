66718f2977321fb09a3591a30c8fe6ea12a5b8ea
xktime <luoliwen0913@foxmail.com>
2022-09-24 15:39:52 +0800

    引入配置中心

---
+++ config/src/main/java/com/xktime/config/ReshareConfigApplication.java
@@ -0,0 +1,18 @@
+package com.xktime.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: luoliwen
+ * @Description
+ * @Date: 2022/9/24 14:59
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ReshareConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReshareConfigApplication.class, args);
+    }
+}
