7c55235773a6e5919e33108d98010e0f57e9b000
xktime <luoliwen0913@foxmail.com>
2022-10-19 19:01:40 +0800

    python csdn爬虫

--- service-crawler-python/crawler/spiders/csdn_sprider.py
+++ service-crawler-python/crawler/spiders/csdn_sprider.py
@@ -1,19 +1,27 @@
 import scrapy
-
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 from crawler.items import CrawlerItem
+
 
 class CSDNSpider(scrapy.Spider):
     name = "csdn"
-    allowed_domains = ["dmoz.org"]
-    start_urls = [
-        "https://www.csdn.net/"
-    ]
+    allowed_domains = ["blog.csdn.net"]
+    start_urls = ["https://www.csdn.net/"]
 
     def parse(self, response):
+        print("parse start")
         for sel in response.xpath('//ul/li'):
             link = sel.xpath('a/@href').extract()
             if len(link) == 0:
                 continue
+            req = scrapy.Request(str(link[0]), callback=self.parse_details)
+            yield req
+
+    def parse_details(self, response):
+        print("parse_details start")
+        for url in response.xpath("//a[@ class = 'blog']/@href").getall():
             item = CrawlerItem()
-            item["url"] = link
+            item["url"] = url
             yield item
+
